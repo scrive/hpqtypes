8b4d57876dc573759c2935064306806b793844c5
Andrzej Rybczak <electricityispower@gmail.com>
2014-10-05 16:22:29 +0200

    generic MonadDB instance: lift folds using MonadTransControl

--- src/Database/PostgreSQL/PQTypes/Class/Instances/Overlapping.hs
+++ src/Database/PostgreSQL/PQTypes/Class/Instances/Overlapping.hs
@@ -1,22 +1,22 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 {-# LANGUAGE FlexibleContexts, FlexibleInstances, OverlappingInstances
-  , UndecidableInstances #-}
+  , Rank2Types, UndecidableInstances #-}
 -- | Definition of generic 'MonadDB' instance which works for
--- any transformer with 'MonadTrans', 'MonadTransControl'
--- and 'MonadBase' 'IO' instances. Quite convenient, but not
--- imported by default due to OverlappingInstances requirement.
-module Database.PostgreSQL.PQTypes.Class.Instances.Overlapping where
+-- any transformer with 'MonadTrans' and 'MonadTransControl'
+-- instances. Quite convenient, but not imported by default
+-- due to OverlappingInstances requirement.
+module Database.PostgreSQL.PQTypes.Class.Instances.Overlapping () where
 
-import Control.Monad.Base
+import Control.Applicative
 import Control.Monad.Trans
 import Control.Monad.Trans.Control
 import Database.PostgreSQL.PQTypes.Class
-import Database.PostgreSQL.PQTypes.Fold
 
 instance (
-    MonadTrans t
+    Applicative (t m)
+  , Monad (t m)
+  , MonadTrans t
   , MonadTransControl t
-  , MonadBase IO (t m)
   , MonadDB m
   ) => MonadDB (t m) where
     runQuery = lift . runQuery
@@ -26,7 +26,15 @@ instance (
     clearQueryResult = lift clearQueryResult
     getTransactionSettings = lift getTransactionSettings
     setTransactionSettings = lift . setTransactionSettings
-    foldlM = foldLeftM
-    foldrM = foldRightM
-    withNewConnection m = restoreT . return
-      =<< liftWith (\run -> withNewConnection (run m))
+    foldlM f acc = controlT $ \run ->
+      run (return acc) >>= foldlM (\acc' row ->
+        run $ restoreT (return acc') >>= flip f row)
+    foldrM f acc = controlT $ \run ->
+      run (return acc) >>= foldrM (\row acc' ->
+        run $ restoreT (return acc') >>= f row)
+    withNewConnection m = controlT $ \run ->
+      withNewConnection (run m)
+
+controlT :: (MonadTransControl t, Monad (t m), Monad m)
+         => (Run t -> m (StT t a)) -> t m a
+controlT f = liftWith f >>= restoreT . return
