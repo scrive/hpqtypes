697d755772ab4d03868812597fbf776ec8fb2131
Tom Feron <tho.feron@gmail.com>
2021-04-20 13:37:47 +0000

    Add runQuery variants ensuring 1 or >0 rows affected

--- src/Database/PostgreSQL/PQTypes/Utils.hs
+++ src/Database/PostgreSQL/PQTypes/Utils.hs
@@ -4,10 +4,14 @@ module Database.PostgreSQL.PQTypes.Utils (
   , runQuery_
   , runQuery01
   , runQuery01_
+  , runQuery1_
+  , runQueryNot0_
   , runSQL
   , runSQL_
   , runSQL01
   , runSQL01_
+  , runSQL1_
+  , runSQLNot0_
   -- Internal.Utils
   , hpqTypesError
   ) where
@@ -67,6 +71,30 @@ runQuery01 sql = do
 runQuery01_ :: (IsSQL sql, MonadDB m, MonadThrow m) => sql -> m ()
 runQuery01_ = void . runQuery01
 
+-- | Specialization of 'runQuery' that checks whether affected/returned
+-- number of rows is exactly 1.
+-- Otherwise, 'AffectedRowsMismatch' exception is thrown.
+{-# INLINABLE runQuery1_ #-}
+runQuery1_ :: (IsSQL sql, MonadDB m, MonadThrow m) => sql -> m ()
+runQuery1_ sql = do
+  n <- runQuery sql
+  when (n /= 1) $ throwDB AffectedRowsMismatch {
+    rowsExpected = [(1, 1)]
+  , rowsDelivered = n
+  }
+
+-- | Specialization of 'runQuery' that checks whether affected/returned
+-- number of rows is greater than 0.
+-- Otherwise, 'AffectedRowsMismatch' exception is thrown.
+{-# INLINABLE runQueryNot0_ #-}
+runQueryNot0_ :: (IsSQL sql, MonadDB m, MonadThrow m) => sql -> m ()
+runQueryNot0_ sql = do
+  n <- runQuery sql
+  when (n == 0) $ throwDB AffectedRowsMismatch {
+    rowsExpected = []
+  , rowsDelivered = 0
+  }
+
 ----------------------------------------
 
 -- | Specialization of 'runQuery' to 'SQL' type.
@@ -88,3 +116,13 @@ runSQL01 = runQuery01
 {-# INLINABLE runSQL01_ #-}
 runSQL01_ :: (MonadDB m, MonadThrow m) => SQL -> m ()
 runSQL01_ = runQuery01_
+
+-- | Specialization of 'runQuery1_' to 'SQL' type.
+{-# INLINABLE runSQL1_ #-}
+runSQL1_ :: (MonadDB m, MonadThrow m) => SQL -> m ()
+runSQL1_ = runQuery1_
+
+-- | Specialization of 'runQueryNot0_' to 'SQL' type.
+{-# INLINABLE runSQLNot0_ #-}
+runSQLNot0_ :: (MonadDB m, MonadThrow m) => SQL -> m ()
+runSQLNot0_ = runQuery01_
