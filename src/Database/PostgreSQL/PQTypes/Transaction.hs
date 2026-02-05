e2a379a883f6ab9a11710e6e028346464ca87ee9
Fredrik Carlén <fredrik@scrive.com>
2026-02-05 14:55:35 +0100

    Cosmetic update: only import instances from Database.PostgreSQL.PQTypes.Internal.Error

--- src/Database/PostgreSQL/PQTypes/Transaction.hs
+++ src/Database/PostgreSQL/PQTypes/Transaction.hs
@@ -13,7 +13,7 @@ import GHC.Stack
 
 import Data.Monoid.Utils
 import Database.PostgreSQL.PQTypes.Class
-import Database.PostgreSQL.PQTypes.Internal.Error
+import Database.PostgreSQL.PQTypes.Internal.Error()
 import Database.PostgreSQL.PQTypes.SQL.Raw
 import Database.PostgreSQL.PQTypes.Transaction.Settings
 import Database.PostgreSQL.PQTypes.Utils
